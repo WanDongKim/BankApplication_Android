ae076b6c8623816d450fb1f530adf84af47fa0c4
WanDongKim <zeumes12@gmail.com>
2017-10-23 13:51:51 -0400

    Created Search Account Activity

---
+++ app/src/main/java/com/example/mrgim/bankapplication/SearchAccountActivity.java
@@ -0,0 +1,13 @@
+package com.example.mrgim.bankapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_account);
+    }
+}
