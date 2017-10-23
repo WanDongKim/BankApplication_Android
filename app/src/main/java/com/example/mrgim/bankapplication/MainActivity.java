ad283313466801052081b126a097cbfcf13694c4
WanDongKim <zeumes12@gmail.com>
2017-10-23 12:30:04 -0400

    Override onClickListener

--- app/src/main/java/com/example/mrgim/bankapplication/MainActivity.java
+++ app/src/main/java/com/example/mrgim/bankapplication/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.mrgim.bankapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCreateAcc = (Button) findViewById(R.id.btnCreateAccount);
+        Button btnSearchAcc = (Button) findViewById(R.id.btnSearchAccount);
+        Button btnAdmin = (Button) findViewById(R.id.btnAdminMode);
+        Button btnExit = (Button) findViewById(R.id.btnExit);
+
+        btnCreateAcc.setOnClickListener(this);
+        btnSearchAcc.setOnClickListener(this);
+        btnAdmin.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnCreateAccount:
+                break;
+            case R.id.btnSearchAccount:
+                break;
+            case R.id.btnAdminMode:
+                break;
+            case R.id.btnExit:
+                break;
+        }
     }
 }
